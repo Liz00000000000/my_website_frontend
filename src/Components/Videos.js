6d13dbba8c1c62bb3101cbb22ab4782d5832a0ec
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-27 16:42:16 -0400

    videos

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Video = () => {
-    return <div>Videos</div>
+    return (
+        <div>
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/tzsZxJwcoKI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/DdPL-dB-62A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/-kHoIrHSYFo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/euEMBbeC62U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    )
 }
 
 export default Video 
